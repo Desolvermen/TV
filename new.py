e0a6fc96e2259deb0a0056160419d03ebbc3a3dd
Desolvermen <Nelver@bk.ru>
2025-08-24 11:29:56 +0300

    Update new.py

--- new.py
+++ new.py
@@ -18,6 +18,22 @@ wanted_channels = [
     "МузТВ"
 ]
 
+# Функция для получения HLS ссылки из API Rutube
+def get_rutube_hls_url(api_url):
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        data = response.json()
+        # Извлекаем URL HLS из ответа
+        return data['live_streams']['hls'][0]['url'] if 'live_streams' in data and 'hls' in data['live_streams'] else None
+    else:
+        print('Ошибка при запросе к API Rutube:', response.status_code)
+        return None
+
+# URL API для получения HLS ссылки
+rutube_api_url = 'https://rutube.ru/api/play/options/c9b87c0b00cfff9b37f95b9c8e4eed42/'
+# Получаем HLS ссылку
+soloviev_hls_url = get_rutube_hls_url(rutube_api_url)
+
 # Загружаем плейлист
 response = requests.get(playlist_url)
 if response.status_code == 200:
@@ -32,9 +48,13 @@ filtered_lines = []
 # Добавляем заголовок M3U
 filtered_lines.append('#EXTM3U')
 
-# Сначала добавляем статичный канал Соловьёв Live HD
-filtered_lines.append('#EXTINF:-1,Соловьёв Live HD')
-filtered_lines.append('http://player.smotrim.ru/iframe/stream/live_id/985d5c7b-9727-4942-a4ba-a6e852caf0c1.m3u8')
+# Проверяем, получена ли HLS ссылка
+if soloviev_hls_url:
+    # Добавляем канал Соловьёв Live HD с динамической HLS ссылкой
+    filtered_lines.append('#EXTINF:-1,Соловьёв Live HD')
+    filtered_lines.append(soloviev_hls_url)
+else:
+    print('Не удалось получить HLS ссылку для Соловьёв Live HD')
 
 # Обработка строк плейлиста
 for channel in wanted_channels:
@@ -78,6 +98,10 @@ for channel in wanted_channels:
                     filtered_lines.append('http://tvchannelstream1.tvzvezda.ru/cdn/tvzvezda/playlist_hdhigh.m3u8')
                     filtered_lines.append('#EXTINF:-1,Звезда Плюс HD')
                     filtered_lines.append('http://tvzvezda.bonus-tv.ru/cdn/zvezdaplus/mono.ts.m3u8')
+                    filtered_lines.append('#EXTINF:-1,Мир HD')
+                    filtered_lines.append('http://hls-mirtv.cdnvideo.ru/mirtv-parampublish/mirtv_2500/tracks-v1a1/mono.m3u8')
+                    filtered_lines.append('#EXTINF:-1,Мир 24 HD')
+                    filtered_lines.append('http://hls-mirtv.cdnvideo.ru/mirtv-parampublish/mir24_2500/tracks-v1a1/mono.m3u8')
                 break  # Выйти из внешнего цикла после добавления канала
 
 # Запись отфильтрованного плейлиста в файл с учетом кодировки
